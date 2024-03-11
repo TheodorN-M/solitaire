548c8988f5515cf5c7355ba20d12b52e810b0569
Theodor <theodornm@gmail.com>
2024-03-11 14:19:47 +0100

    Added method to place up cards for completion

--- src/main/java/com/theodor/app/SolitaireTable.java
+++ src/main/java/com/theodor/app/SolitaireTable.java
@@ -1,23 +1,48 @@
 package com.theodor.app;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 import com.theodor.app.cards.Card;
+import com.theodor.app.cards.CardDeck;
+import com.theodor.app.cards.CardSuit;
 import com.theodor.app.grid.CellPosition;
 import com.theodor.app.grid.Grid;
 
-public class SolitaireTable extends Grid<Card>{
+public class SolitaireTable extends Grid<Card> {
+
+    private CardDeck toDrawFrom;
+    private HashMap<CardSuit, LinkedList<Card>> completed;
 
     public SolitaireTable() {
         super(20, 7);
-
+        this.completed = new HashMap<>();
     }
-    
-    public boolean emptyCol(int col){
+
+    public void putAway(Card card){
+        CardSuit thisSuit = card.getSuit();
+        LinkedList<Card> pile = completed.getOrDefault(thisSuit, new LinkedList<Card>());
+        // If the pile is empty and the card to stow is an ace
+        if (pile.isEmpty() && card.getValue() == 1){ 
+            pile.add(card);
+            completed.put(thisSuit, pile);
+            return;
+        } 
+        // If top card is one less than the card in question's value
+        if (!pile.isEmpty() && pile.getLast().getValue() +1 == card.getValue()){
+            pile.add(card);
+            completed.put(thisSuit, pile);
+            return;
+        }
+        
+    }
+
+    public boolean emptyCol(int col) {
         return getCardsInCol(col).isEmpty();
     }
-    
-    private ArrayList<Card> getCardsInCol(int col){
+
+    private ArrayList<Card> getCardsInCol(int col) {
         ArrayList<Card> cardsInCol = new ArrayList<>();
         for (int i = 0; i < rows(); i++) {
             Card card = get(new CellPosition(i, col));
@@ -27,7 +52,7 @@ public class SolitaireTable extends Grid<Card>{
         return cardsInCol;
     }
 
-    public void printCol(int col){
+    public void printCol(int col) {
         for (Card card : getCardsInCol(col)) {
             System.out.println(card.toString());
         }
@@ -35,7 +60,7 @@ public class SolitaireTable extends Grid<Card>{
 
     public void print() {
         int maxRows = 7;
-    
+
         // Iterate over each row
         for (int row = 0; row < maxRows; row++) {
             // Then iterate over each column in that row
@@ -47,14 +72,12 @@ public class SolitaireTable extends Grid<Card>{
                         System.out.print(card.toString() + "\t"); // Use tab for spacing
                     else
                         System.out.print("----------\t");
-                } 
-                else 
+                } else
                     System.out.print("\t\t"); // Print extra spaces if this column is shorter
-                
+
             }
             System.out.println(); // Move to the next line after printing all columns in this row
         }
     }
-    
 
 }
