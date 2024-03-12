bf301d6eeee6f6db98a8671fafbd98b3e3f6be42
Theodor <theodornm@gmail.com>
2024-03-12 13:05:04 +0100

    Solving algorithm now looks for and moves cards on the table

--- src/main/java/com/theodor/app/algorithm/BruteSolver.java
+++ src/main/java/com/theodor/app/algorithm/BruteSolver.java
@@ -22,29 +22,48 @@ public class BruteSolver {
         while (!game.isFinished()) {
             for (GridCell<Card> gridCell : table) {
                 Card currentCard = gridCell.getValue();
-                CellPosition currentPos = gridCell.getPos(); 
+                CellPosition currentPos = gridCell.getPos();
 
-                if (currentCard == null || !currentCard.faceUp() )
+                if (currentCard == null || !currentCard.faceUp())
                     continue;
-                if (table.stowAway(currentCard)){
+                if (table.stowAway(currentCard)) {
                     table.set(currentPos, null);
-                    int currentRow = currentPos.getRow()-1;
+                    int currentRow = currentPos.getRow() - 1;
                     if (currentRow < 0)
                         continue;
 
                     Card cardBeneath = table.get(new CellPosition(currentRow, currentPos.getCol()));
-                    if (!cardBeneath.faceUp())
+                    if (!cardBeneath.faceUp()) {
                         cardBeneath.flip();
-                    
-                    
+                        continue;
+                    }
+
+                }
+
+                for (GridCell<Card> gridCell2 : table) {
+
+                    Card other = gridCell2.getValue();
+
+                    if (other == null && currentCard.getValue() == 13){
+                        table.moveKing(gridCell, gridCell2.getPos());
+                        break;
+                    }
+
+                    if (other == null || currentCard.equals(other))
+                        continue;
+
+                    if (game.isLegalPlacement(other, currentCard))
+                        table.move(gridCell2, gridCell);
                 }
 
                 table.print();
             }
-            break;
+
+            timeElapsed = System.currentTimeMillis() - startTime;
+            if (timeElapsed > 100)
+                break;
         }
-        timeElapsed = System.currentTimeMillis() - startTime;
         System.out.println("Time elapsed: " + timeElapsed + " ms");
-        return true;
+        return false;
     }
 }
