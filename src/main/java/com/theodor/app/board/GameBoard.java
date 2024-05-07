dffcf3b21a107b8eabf635300780dd9c3e2593d6
Theodor <theodornm@gmail.com>
2024-05-07 09:04:18 +0200

    Changes in board logic, work (a lot) is needed

---
+++ src/main/java/com/theodor/app/board/GameBoard.java
@@ -0,0 +1,13 @@
+package com.theodor.app.board;
+
+import java.util.ArrayList;
+
+public class GameBoard {
+    private GameBoard successor;
+    private ArrayList<GameBoard> predecessors;
+
+    public GameBoard(GameBoard current){
+        this.successor = current;
+    }
+
+}
---
+++ src/main/java/com/theodor/app/board/GameHistory.java
@@ -0,0 +1,66 @@
+package com.theodor.app.board;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class GameHistory implements Collection<GameBoard> {
+
+    @Override
+    public boolean contains(Object o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'contains'");
+    }
+
+    @Override
+    public Iterator<GameBoard> iterator() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+    }
+
+    @Override
+    public Object[] toArray() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toArray'");
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toArray'");
+    }
+
+    @Override
+    public boolean add(GameBoard e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'containsAll'");
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends GameBoard> c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addAll'");
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'removeAll'");
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'retainAll'");
+    }
+
+   
+
+}
